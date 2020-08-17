9da91ec2b62d20b93cae56d74e6d8606fcd1e5f2
shivakumarjava <Shivaragav20@gmail.com>
2020-08-17 22:28:26 +0530

    baseclass added

---
+++ src/test/java/utils/Baseclass.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Baseclass {
+	
+	public void getdata()
+	{
+		System.out.println("getdata");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
